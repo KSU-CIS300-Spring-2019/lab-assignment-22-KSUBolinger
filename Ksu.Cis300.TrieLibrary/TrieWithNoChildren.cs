833e3632f3262cc63eefe136f8b718b4e146e79b
Easton Bolinger <bolinger@ksu.edu>
2019-03-25 15:10:36 -0500

    Lab 22 Attempted

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithNoChildren : ITrie
+    {
+        private bool _contains = false;
+        public ITrie Add(string s)
+        {
+           if(s == "")
+            {
+                _contains = true;
+            }
+            else
+            {
+                return new TrieWithOneChild(s, _contains);
+            }
+            return this;
+        }
+
+        public bool Contains(string s)
+        {
+            if(s == "")
+            {
+                _contains = true;
+            }
+            else
+            {
+                return false;
+            }
+            return _contains;
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithOneChild : ITrie
+    {
+        private bool _doesContain;
+        private ITrie _onlyChild;
+        private char _label;
+        public ITrie Add(string s)
+        {
+            if(s == "")
+            {
+                _doesContain = true;
+                return this;
+            }
+            else if()
+        }
+
+        public bool Contains(string s)
+        {
+            if(s == "")
+            {
+                return _doesContain;
+            }
+            return false;
+        }
+
+        public TrieWithOneChild(string s, bool isEmpty)
+        {
+           if(s == "" || s[0] > 'a' || s[0] < 'z')
+            {
+                throw new ArgumentException();
+            }
+            _doesContain = isEmpty;
+            _label = s[0];
+            _onlyChild = new TrieWithNoChildren().Add(s.Substring(1));
+        } 
+    }
+}
